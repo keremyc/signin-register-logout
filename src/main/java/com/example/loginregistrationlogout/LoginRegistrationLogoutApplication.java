0bfbe5c8576264cb573f4fc99c1a04be7bb71aa6
Kerem <keremyc.yc@gmail.com>
2021-02-14 17:52:39 +0300

    Created project via Spring initializer.

---
+++ src/main/java/com/example/loginregistrationlogout/LoginRegistrationLogoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginregistrationlogout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationLogoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationLogoutApplication.class, args);
+	}
+
+}
